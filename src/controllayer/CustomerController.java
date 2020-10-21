47ce6e67bd56f0d669c2ff9bb60861538e822606
SimonNiese <1086234@ucn.dk>
2020-10-21 09:59:36 +0200

    Added CustomerController

--- src/controllayer/CustomerController.java
+++ src/controllayer/CustomerController.java
@@ -1,5 +1,19 @@
 package controllayer;
 
-public class CustomerController {
+import databaselayer.CustomerDBIF;
+import modellayer.Customer;
 
+public class CustomerController {
+	private CustomerDBIF customerDB;
+	private Customer customer;
+	
+	public CustomerController(CustomerDBIF customerDB, Customer customer) {
+		super();
+		this.customerDB = customerDB;
+		this.customer = customer;
+	}
+	
+	public Customer findCustomer(int cId) {
+		return customerDB.findCustomer(cID);
+	}
 }
