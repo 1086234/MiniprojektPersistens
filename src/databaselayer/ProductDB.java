8b51aa333d1c51aa20c810f75a41bb7bd1e7966a
ThomasHenriksen <70630520+ThomasHenriksen@users.noreply.github.com>
2020-10-21 09:46:30 +0200

    Product

--- src/databaselayer/ProductDB.java
+++ src/databaselayer/ProductDB.java
@@ -1,5 +1,107 @@
 package databaselayer;
 
-public class ProductDB {
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
 
+import controllayer.DataAccessException;
+
+import modellayer.*;
+
+public class ProductDB implements ProductDBIF {
+	private Product product;
+	private static final String FIND_BY_VARENO = "select * from product where vareNo = ?";
+	private PreparedStatement findByVareNo;
+	private static final String UPDATE_STOCK_BY_VARENO = "update product stock = ? where vareNo = ?";
+	private PreparedStatement updateStockByVareNO;
+
+	public ProductDB() throws DataAccessException {
+
+		init();
+	}
+
+	public void init() throws DataAccessException {
+		Connection con = DBConnection.getInstance().getConnection();
+		try {
+			findByVareNo = con.prepareStatement(FIND_BY_VARENO);
+			updateStockByVareNO = con.prepareStatement(UPDATE_STOCK_BY_VARENO);
+		} catch (SQLException e) {
+			throw new DataAccessException(DBMessages.COULD_NOT_PREPARE_STATEMENT, e);
+
+		}
+	}
+
+	@Override
+	public Product findByVareNo(int id) throws DataAccessException {
+		Product res = null;
+		try {
+			findByVareNo.setInt(1, id);
+			ResultSet rs = findByVareNo.executeQuery();
+			if (rs.next()) {
+				res = buildObject(rs);
+			}
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			throw new DataAccessException(DBMessages.COULD_NOT_BIND_OR_EXECUTE_QUERY, e);
+
+		}
+		return res;
+	}
+
+	@Override
+	public Product updateStock(Product product, int quintity) throws DataAccessException{
+		Product res = null;
+		
+		try {
+			int vareNo = product.getVareNo();
+			updateStockByVareNO.setInt(2, vareNo);
+			updateStockByVareNO.setInt(1, quintity);
+			ResultSet rs = updateStockByVareNO.executeQuery();
+			if(rs.next()) {
+				res = buildObject(rs);
+			}
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			throw new DataAccessException(DBMessages.COULD_NOT_BIND_OR_EXECUTE_QUERY, e);
+			
+		}
+		return res;
+	}
+	
+	private List<Product> buildObjects(ResultSet rs) throws DataAccessException {
+		List<Product> res = new ArrayList<>();
+		try {
+			while (rs.next()) {
+				Product currPerson = buildObject(rs);
+				res.add(currPerson);
+			}
+		} catch (SQLException e) {
+			// e.printStackTrace();
+			throw new DataAccessException(DBMessages.COULD_NOT_READ_RESULTSET, e);
+		}
+		return res;
+
+	}
+
+	private Product buildObject(ResultSet rs) throws DataAccessException {
+		Product product = new Product();
+		try {
+			product.setVareNo(rs.getInt("vareNo"));
+			product.setName(rs.getString("name"));
+			product.setSalesPrice(rs.getDouble("phone"));
+			product.setCountryOfOrigin(rs.getString("birthDate"));
+			product.setStock(rs.getInt("stock"));
+			product.setMinStock(rs.getInt("minStock"));
+			product.setMaxStock(rs.getInt("MaxStock"));
+
+		} catch (SQLException e) {
+			throw new DataAccessException(DBMessages.COULD_NOT_READ_RESULTSET, e);
+
+		}
+		return product;
+	}
 }
--- src/modellayer/Product.java
+++ src/modellayer/Product.java
@@ -1,5 +1,137 @@
 package modellayer;
 
 public class Product {
+	private int vareNo;
+	private String name;
+	private double salesPrice;
+	private String countryOfOrigin;
+	private int stock;
+	private int minStock;
+	private int maxStock;
 
+	/**
+	 * @param vareNo
+	 * @param name
+	 * @param salesPrice
+	 * @param countryOfOrigin
+	 * @param stock
+	 * @param minStock
+	 * @param maxStock
+	 */
+	public Product() {
+	}
+
+	public Product(int vareNo) {
+		this.vareNo = vareNo;
+	}
+
+	public Product(int vareNo, String name, double salesPrice, String countryOfOrigin, int stock, int minStock,
+			int maxStock) {
+
+		this.vareNo = vareNo;
+		this.name = name;
+		this.salesPrice = salesPrice;
+		this.countryOfOrigin = countryOfOrigin;
+		this.stock = stock;
+		this.minStock = minStock;
+		this.maxStock = maxStock;
+	}
+
+	/**
+	 * @return the vareNo
+	 */
+	public int getVareNo() {
+		return vareNo;
+	}
+
+	/**
+	 * @param vareNo the vareNo to set
+	 */
+	public void setVareNo(int vareNo) {
+		this.vareNo = vareNo;
+	}
+
+	/**
+	 * @return the name
+	 */
+	public String getName() {
+		return name;
+	}
+
+	/**
+	 * @param name the name to set
+	 */
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	/**
+	 * @return the salesPrice
+	 */
+	public double getSalesPrice() {
+		return salesPrice;
+	}
+
+	/**
+	 * @param salesPrice the salesPrice to set
+	 */
+	public void setSalesPrice(double salesPrice) {
+		this.salesPrice = salesPrice;
+	}
+
+	/**
+	 * @return the countryOfOrigin
+	 */
+	public String getCountryOfOrigin() {
+		return countryOfOrigin;
+	}
+
+	/**
+	 * @param countryOfOrigin the countryOfOrigin to set
+	 */
+	public void setCountryOfOrigin(String countryOfOrigin) {
+		this.countryOfOrigin = countryOfOrigin;
+	}
+
+	/**
+	 * @return the stock
+	 */
+	public int getStock() {
+		return stock;
+	}
+
+	/**
+	 * @param stock the stock to set
+	 */
+	public void setStock(int stock) {
+		this.stock = stock;
+	}
+
+	/**
+	 * @return the minStock
+	 */
+	public int getMinStock() {
+		return minStock;
+	}
+
+	/**
+	 * @param minStock the minStock to set
+	 */
+	public void setMinStock(int minStock) {
+		this.minStock = minStock;
+	}
+
+	/**
+	 * @return the maxStock
+	 */
+	public int getMaxStock() {
+		return maxStock;
+	}
+
+	/**
+	 * @param maxStock the maxStock to set
+	 */
+	public void setMaxStock(int maxStock) {
+		this.maxStock = maxStock;
+	}
 }
