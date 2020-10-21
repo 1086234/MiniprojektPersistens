083a6fe20192d46b37b74c7e085f612d9f5b6493
InvisibleOlive <70630509+InvisibleOlive@users.noreply.github.com>
2020-10-21 10:36:07 +0200

    Update SaleOrderLine.java

--- src/modellayer/SaleOrderLine.java
+++ src/modellayer/SaleOrderLine.java
@@ -1,5 +1,44 @@
 package modellayer;
 
 public class SaleOrderLine {
+	int quantity;
+	Product product;
+	
+	/**
+	 * @param quantity
+	 * @param product
+	 */
+	public SaleOrderLine(int quantity, Product product) {
+		super();
+		this.quantity = quantity;
+		this.product = product;
+	}
+	
+	/**
+	 * @return the quantity
+	 */
+	public int getQuantity(Product product) {
+		return quantity;
+	}
 
+	/**
+	 * @param quantity the quantity to set
+	 */
+	public void setQuantity(int quantity) {
+		this.quantity = quantity;
+	}
+	/**
+	 * @return the quantity
+	 */
+	public Product getProduct(Product product){
+		return product;
+	}
+
+	/**
+	 * @param quantity the quantity to set
+	 */
+	public void setProduct(Product product) {
+		this.product = product;
+	}
+	
 }
