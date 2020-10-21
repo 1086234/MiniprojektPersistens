b1d55bd75d4446992663479d07bfce5d707ed1c6
InvisibleOlive <70630509+InvisibleOlive@users.noreply.github.com>
2020-10-21 10:04:45 +0200

    Prøver igen

--- src/controllayer/SaleOrderController.java
+++ src/controllayer/SaleOrderController.java
@@ -1,5 +1,28 @@
 package controllayer;
 
-public class SaleOrderController {
+import modellayer.Product;
+import modellayer.SaleOrder;
+import modellayer.SaleOrderLine;
 
+public class SaleOrderController {
+	private SaleOrder saleOrder;
+	private SaleOrderLine saleOrderLine;
+	private ProductController productController;
+	private CustomerController customerController;
+	
+	public void createOrder() {
+		
+	}
+	
+	public void addProduct(Product product, int quantity) {
+		
+	}
+	
+	public void addCustomer(int cId){
+		
+	}
+	
+	public void endOrder() {
+		
+	}
 }
