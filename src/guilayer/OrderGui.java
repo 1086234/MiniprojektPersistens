e9fe695bf4f7f79e0ca334fd971d34b13a27e1cd
ThomasHenriksen <70630520+ThomasHenriksen@users.noreply.github.com>
2020-10-23 08:41:25 +0200

    totalPrices Fix

--- src/guilayer/OrderGui.java
+++ src/guilayer/OrderGui.java
@@ -30,12 +30,14 @@ import java.util.List;
 import javax.swing.JButton;
 import java.awt.event.ActionListener;
 import java.sql.SQLException;
+import java.awt.Font;
 
 public class OrderGui extends JFrame {
 	private SaleOrderController saleOrderController;
 	private SaleOrderLineListTableModel solltm;
 	private JTable tableOrder;
 
+	private JLabel lblTotalPrices;
 	private JPanel contentPane;
 	private JTextField textFieldNavn;
 	private JTextField textFieldAddress;
@@ -69,7 +71,7 @@ public class OrderGui extends JFrame {
 	public OrderGui() throws DataAccessException {
 
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 1000, 600);
+		setBounds(100, 100, 1200, 700);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
@@ -110,30 +112,19 @@ public class OrderGui extends JFrame {
 		JLabel lblZipCode = new JLabel("PostNummer :");
 
 		JLabel lblCity = new JLabel("by :");
-		panel.setLayout(new FlowLayout(FlowLayout.CENTER, 5, 5));
-		panel.add(lblCustomerID);
 
 		textFieldCustomerID = new JTextField();
 		textFieldCustomerID.setText("");
 		textFieldCustomerID.setColumns(10);
-		panel.add(textFieldCustomerID);
-		panel.add(lblCustomerName);
 
 		textFieldNavn = new JTextField();
 		textFieldNavn.setColumns(10);
-		panel.add(textFieldNavn);
-		panel.add(lblCity);
 
 		textFieldCity = new JTextField();
 		textFieldCity.setColumns(10);
-		panel.add(textFieldCity);
-		panel.add(lblAddress);
 
 		textFieldAddress = new JTextField();
 		textFieldAddress.setColumns(10);
-		panel.add(textFieldAddress);
-		panel.add(lblZipCode);
-		panel.add(textFieldZipCode);
 
 		JPanel panel_Order = new JPanel();
 
@@ -172,29 +163,84 @@ public class OrderGui extends JFrame {
 			}
 		});
 		GroupLayout gl_contentPane = new GroupLayout(contentPane);
-		gl_contentPane.setHorizontalGroup(gl_contentPane.createParallelGroup(Alignment.LEADING).addGroup(gl_contentPane
-				.createSequentialGroup()
-				.addComponent(panel, GroupLayout.PREFERRED_SIZE, 102, GroupLayout.PREFERRED_SIZE).addGap(10)
-				.addGroup(gl_contentPane.createParallelGroup(Alignment.TRAILING)
-						.addComponent(panel_1, Alignment.LEADING, GroupLayout.DEFAULT_SIZE, 802, Short.MAX_VALUE)
-						.addGroup(gl_contentPane.createSequentialGroup().addComponent(btnRemoveOneOrderLine).addGap(18)
-								.addComponent(btnCancal).addGap(18).addComponent(btnPay))
-						.addGroup(gl_contentPane.createSequentialGroup().addPreferredGap(ComponentPlacement.UNRELATED)
-								.addComponent(panel_Order, GroupLayout.DEFAULT_SIZE, 852, Short.MAX_VALUE)))
-				.addContainerGap()));
-		gl_contentPane
-				.setVerticalGroup(gl_contentPane.createParallelGroup(Alignment.LEADING)
-						.addGroup(Alignment.TRAILING, gl_contentPane.createSequentialGroup().addGap(5)
-								.addComponent(panel_1, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE,
-										GroupLayout.PREFERRED_SIZE)
-								.addPreferredGap(ComponentPlacement.RELATED)
-								.addComponent(panel_Order, GroupLayout.DEFAULT_SIZE, 458, Short.MAX_VALUE)
-								.addPreferredGap(ComponentPlacement.RELATED)
-								.addGroup(gl_contentPane.createParallelGroup(Alignment.BASELINE).addComponent(btnPay)
-										.addComponent(btnCancal).addComponent(btnRemoveOneOrderLine))
-								.addGap(23))
+		gl_contentPane.setHorizontalGroup(
+			gl_contentPane.createParallelGroup(Alignment.LEADING)
+				.addGroup(gl_contentPane.createSequentialGroup()
+					.addComponent(panel, GroupLayout.PREFERRED_SIZE, 165, Short.MAX_VALUE)
+					.addPreferredGap(ComponentPlacement.RELATED)
+					.addGroup(gl_contentPane.createParallelGroup(Alignment.TRAILING)
+						.addComponent(panel_1, GroupLayout.DEFAULT_SIZE, 993, Short.MAX_VALUE)
 						.addGroup(gl_contentPane.createSequentialGroup()
-								.addComponent(panel, GroupLayout.DEFAULT_SIZE, 535, Short.MAX_VALUE).addGap(16)));
+							.addGap(715)
+							.addComponent(btnRemoveOneOrderLine)
+							.addGap(18)
+							.addComponent(btnCancal)
+							.addGap(18)
+							.addComponent(btnPay))
+						.addGroup(gl_contentPane.createSequentialGroup()
+							.addGap(10)
+							.addComponent(panel_Order, GroupLayout.DEFAULT_SIZE, 983, Short.MAX_VALUE)))
+					.addContainerGap())
+		);
+		gl_contentPane.setVerticalGroup(
+			gl_contentPane.createParallelGroup(Alignment.LEADING)
+				.addGroup(gl_contentPane.createSequentialGroup()
+					.addGap(5)
+					.addComponent(panel_1, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
+					.addPreferredGap(ComponentPlacement.RELATED)
+					.addComponent(panel_Order, GroupLayout.DEFAULT_SIZE, 558, Short.MAX_VALUE)
+					.addPreferredGap(ComponentPlacement.RELATED)
+					.addGroup(gl_contentPane.createParallelGroup(Alignment.BASELINE)
+						.addComponent(btnPay)
+						.addComponent(btnCancal)
+						.addComponent(btnRemoveOneOrderLine))
+					.addGap(23))
+				.addGroup(gl_contentPane.createSequentialGroup()
+					.addComponent(panel, GroupLayout.DEFAULT_SIZE, 635, Short.MAX_VALUE)
+					.addGap(16))
+		);
+		GroupLayout gl_panel = new GroupLayout(panel);
+		gl_panel.setHorizontalGroup(
+				gl_panel.createParallelGroup(Alignment.LEADING)
+						.addGroup(gl_panel.createSequentialGroup().addGap(20)
+								.addGroup(gl_panel.createParallelGroup(Alignment.LEADING).addComponent(lblCustomerID)
+										.addComponent(textFieldCustomerID, GroupLayout.PREFERRED_SIZE,
+												GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
+										.addComponent(lblCustomerName)
+										.addComponent(textFieldNavn, GroupLayout.PREFERRED_SIZE,
+												GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
+										.addComponent(lblCity)
+										.addComponent(textFieldCity, GroupLayout.PREFERRED_SIZE,
+												GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
+										.addComponent(lblAddress)
+										.addComponent(textFieldAddress, GroupLayout.PREFERRED_SIZE,
+												GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
+										.addComponent(lblZipCode).addComponent(textFieldZipCode,
+												GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE,
+												GroupLayout.PREFERRED_SIZE))
+								.addGap(40)));
+		gl_panel.setVerticalGroup(gl_panel.createParallelGroup(Alignment.LEADING)
+				.addGroup(gl_panel.createSequentialGroup().addGap(63).addComponent(lblCustomerID)
+						.addPreferredGap(ComponentPlacement.RELATED)
+						.addComponent(textFieldCustomerID, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE,
+								GroupLayout.PREFERRED_SIZE)
+						.addPreferredGap(ComponentPlacement.RELATED).addComponent(lblCustomerName)
+						.addPreferredGap(ComponentPlacement.RELATED)
+						.addComponent(textFieldNavn, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE,
+								GroupLayout.PREFERRED_SIZE)
+						.addPreferredGap(ComponentPlacement.RELATED).addComponent(lblCity)
+						.addPreferredGap(ComponentPlacement.RELATED)
+						.addComponent(textFieldCity, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE,
+								GroupLayout.PREFERRED_SIZE)
+						.addPreferredGap(ComponentPlacement.RELATED).addComponent(lblAddress)
+						.addPreferredGap(ComponentPlacement.RELATED)
+						.addComponent(textFieldAddress, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE,
+								GroupLayout.PREFERRED_SIZE)
+						.addPreferredGap(ComponentPlacement.RELATED).addComponent(lblZipCode)
+						.addPreferredGap(ComponentPlacement.RELATED).addComponent(textFieldZipCode,
+								GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
+						.addGap(334)));
+		panel.setLayout(gl_panel);
 
 		JLabel lblQuantity = new JLabel("Antal ");
 		panel_1.add(lblQuantity);
@@ -214,13 +260,28 @@ public class OrderGui extends JFrame {
 		});
 
 		textFieldQuantity.setColumns(10);
+
+		JLabel lblkr = new JLabel("Kr.");
+		lblkr.setFont(new Font("Tahoma", Font.PLAIN, 30));
+
+		lblTotalPrices = new JLabel("");
+		lblTotalPrices.setFont(new Font("Tahoma", Font.PLAIN, 36));
 		GroupLayout gl_panel_Order = new GroupLayout(panel_Order);
 		gl_panel_Order.setHorizontalGroup(gl_panel_Order.createParallelGroup(Alignment.LEADING)
-				.addComponent(scrollPane_Order, GroupLayout.DEFAULT_SIZE, 852, Short.MAX_VALUE));
-		gl_panel_Order.setVerticalGroup(gl_panel_Order.createParallelGroup(Alignment.LEADING)
-				.addGroup(gl_panel_Order.createSequentialGroup().addContainerGap()
-						.addComponent(scrollPane_Order, GroupLayout.PREFERRED_SIZE, 394, GroupLayout.PREFERRED_SIZE)
-						.addContainerGap(53, Short.MAX_VALUE)));
+				.addGroup(gl_panel_Order.createSequentialGroup().addContainerGap(10, Short.MAX_VALUE)
+						.addGroup(gl_panel_Order.createParallelGroup(Alignment.LEADING)
+								.addComponent(scrollPane_Order, Alignment.TRAILING, GroupLayout.PREFERRED_SIZE, 964,
+										GroupLayout.PREFERRED_SIZE)
+								.addGroup(Alignment.TRAILING,
+										gl_panel_Order.createSequentialGroup().addComponent(lblTotalPrices)
+												.addPreferredGap(ComponentPlacement.RELATED).addComponent(lblkr)
+												.addContainerGap()))));
+		gl_panel_Order.setVerticalGroup(gl_panel_Order.createParallelGroup(Alignment.LEADING).addGroup(gl_panel_Order
+				.createSequentialGroup().addContainerGap()
+				.addComponent(scrollPane_Order, GroupLayout.PREFERRED_SIZE, 394, GroupLayout.PREFERRED_SIZE)
+				.addPreferredGap(ComponentPlacement.RELATED, 114, Short.MAX_VALUE).addGroup(gl_panel_Order
+						.createParallelGroup(Alignment.BASELINE).addComponent(lblkr).addComponent(lblTotalPrices))
+				.addContainerGap()));
 		panel_Order.setLayout(gl_panel_Order);
 		contentPane.setLayout(gl_contentPane);
 		init();
@@ -247,6 +308,8 @@ public class OrderGui extends JFrame {
 		this.tableOrder.setModel(solltm);
 		List<SaleOrderLine> data = saleOrderController.getOrderLineList();
 		solltm.setData(data);
+
+		lblTotalPrices.setText(Integer.toString(saleOrderController.CalcTotalPrice()));
 	}
 
 	private void fjernVare() {
@@ -275,12 +338,11 @@ public class OrderGui extends JFrame {
 		if (fundet == 1) {
 			JOptionPane.showMessageDialog(null, "Der er ikke en vare med det varenummer",
 					"InfoBox: " + "Ikke i systemet", JOptionPane.INFORMATION_MESSAGE);
-		}
-		else if (fundet == 2) {
+		} else if (fundet == 2) {
 			JOptionPane.showMessageDialog(null, "Der er ikke nok varer på laget for at lave denne ordre",
 					"InfoBox: " + "Ikke nok på lager", JOptionPane.INFORMATION_MESSAGE);
 		}
-		
+
 		fillTable();
 	}
 
